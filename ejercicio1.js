6de9f0a49965af8434bdd702a114017e420a6ad0
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-07-04 03:12:47 +0100

    1

---
+++ ejercicio1.js
@@ -0,0 +1,45 @@
+/* Ejercicio 1
+
+Dado el siguiente código usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas 
+e imprime por consola el array de categorías.
+
+Ten en cuenta que las categorías no tienen que repetirse.
+
+Es decir, const categorias = ["comedia", "aventura", "acción", "thriller", "animación"]
+
+Para filtrar las categorías puedes ayudarte de la función .includes(). */
+
+const movies = [
+    {
+      title: "Bracula: Condemor II",
+      duration: 192,
+      categories: ["comedia", "aventura"],
+    },
+    {
+      title: "Spider-Man: No Way Home",
+      duration: 122,
+      categories: ["aventura", "acción"],
+    },
+    {
+      title: "The Voices",
+      duration: 223,
+      categories: ["comedia", "thriller"],
+    },
+    {
+      title: "Shrek",
+      duration: 111,
+      categories: ["comedia", "aventura", "animación"],
+    },
+  ];
+
+  const MoviesByCategories = [];
+
+  for (const movie of movies) {
+if (!MoviesByCategories.includes(movie.categories)) {
+
+    MoviesByCategories.push(movie.categories);
+}
+
+  }
+
+  console.log(MoviesByCategories);
